ecfd8a0efa2b6bb75a4092451dcf47d3e0053048
Marcel Greter <marcel.greter@ocbnet.ch>
2015-08-27 01:38:38 +0200

    Add simple but huge nested test (randomly generated)

---
+++ generate.pl
@@ -0,0 +1,185 @@
+﻿use strict;
+use warnings;
+
+my $tags = [
+
+	'DIV',
+	'A',
+	'P',
+	'NAV',
+	'SECTION',
+	'TABLE',
+	'TR',
+	'TD',
+	'FORM',
+	'INPUT',
+	'TEXTAREA',
+
+];
+
+my $classes = [
+	'.important',
+	'.warning',
+	'.closed',
+	'.open',
+];
+
+my $ids = [
+	'#slider',
+	'#carousel',
+	'#statusbar',
+	'#navigation'
+];
+
+my $attribs = [
+	'[src]',
+	'[src~="http"]',
+	'[type="text"]',
+	'[href|="#"]'
+];
+
+my $pseudos = [
+	':active',
+	':hover',
+	':focus',
+];
+
+my $props = [
+	'font-family: arial;',
+	'background: red;',
+];
+
+sub random_item
+{
+	return $_[0]->[int(rand(scalar(@{$_[0]})))];
+}
+
+sub random_property
+{
+
+	my ($size) = @_;
+
+	my @properties;
+
+	for (my $i = 0; $i < $size; ++ $i)
+	{
+		push @properties, random_item($props);
+	}
+
+	return @properties;
+
+}
+
+sub random_compound
+{
+	my ($size) = @_;
+
+	my $selector = '';
+
+	while ($selector eq '') {
+
+		my $has_type = int(rand(100)) < 20;
+		my $has_id = int(rand(100)) < 5;
+		my $has_class = int(rand(100)) < 80;
+		my $has_attrib = int(rand(100)) < 15;
+		my $has_pseudo = int(rand(100)) < 5;
+
+		if ($has_type) { $selector .= random_item($tags); }
+		if ($has_id) { $selector .= random_item($ids); }
+		if ($has_class) { $selector .= random_item($classes); }
+		if ($has_attrib) { $selector .= random_item($attribs); }
+		if ($has_pseudo) { $selector .= random_item($pseudos); }
+
+	}
+
+	return $selector;
+
+}
+
+sub random_complex
+{
+	my ($size) = @_;
+
+	my $selector = '';
+
+	for (my $i = 0; $i < $size; $i ++) {
+
+		my $simple = random_compound;
+
+		my $combinator = int(rand(100));
+
+		if ($combinator < 5) {
+			$combinator = " ~ ";
+		} elsif ($combinator < 10) {
+			$combinator = " > ";
+		} elsif ($combinator < 20) {
+			$combinator = " + ";
+		} else {
+			$combinator = " ";
+		}
+
+		$selector .= $combinator if ($i);
+
+		$selector .= $simple;
+
+	}
+
+	return $selector;
+
+}
+
+sub random_selector
+{
+	my $size = int(rand(4)) + 1;
+	return random_complex($size);
+
+}
+
+sub random_block
+{
+
+	my ($nesting, $n) = @_;
+
+	my $result = '';
+
+	$n = 0 unless $n;
+
+	$result .= ("  " x $n) .
+	      random_selector .
+	      " {\n";
+	my $props = int(rand(6)) + 1;
+	$result .= join("", map {
+		("  " x ($n + 1)) . $_ . "\n";
+	} random_property($props));
+
+	random_block($nesting, $n + 1) if ($n < $nesting);
+
+	$result .= ("  " x $n) . "}\n";
+
+	return $result;
+
+}
+
+my $scss = '';
+
+for (my $i = 0; $i < 10000; $i ++)
+{
+	my $nesting = int(rand(25)) + 1;
+	$scss .= random_block($nesting);
+}
+
+use File::Slurp;
+
+write_file('test/nested.scss', $scss);
+
+system 'gulp sass';
+system 'gulp postcss';
+
+
+my $test2 = read_file('test2/nested.scss');
+my $test3 = read_file('test3/nested.css');
+
+$test2 =~ s/\s*/ /g;
+$test3 =~ s/\s*/ /g;
+
+print (($test2 eq $test3 ? "passed" : "mismatch"), "\n");
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass');
 var post = require('gulp-postcss');
+var nested = require('postcss-nested');
 var importC = require('postcss-import')();
 var variabs = require('postcss-css-variables')();
 
 
 gulp.task('postcss', function() {
-	return gulp.src('test/bootstrap.css')
-		.pipe(post([importC, variabs]))
-		.pipe(gulp.dest('test2'));
+	return gulp.src('test/nested.scss')
+		.pipe(post([importC, variabs, nested]))
+		.pipe(gulp.dest('postcss'));
 })
 
 gulp.task('sass', function() {
-	return gulp.src('bower_components/bootstrap-sass/assets/stylesheets/bootstrap.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('test3'));
+	return gulp.src('test/nested.scss')
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(gulp.dest('sass'));
 })
